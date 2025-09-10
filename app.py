07a050441213e9302aab2e337ec0e902ed67f680
Nithesh117 <nitheshkumarkarthikeyan@gmail.com>
2025-09-11 03:56:41 +0530

    testv2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, I am a Flask App running with CI/CD on Azure!"
+    return "Hello, I am a Flask App !"
 
 if __name__ == '__main__':
     app.run()
